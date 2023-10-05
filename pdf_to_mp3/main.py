3797bd6e0f55470f4bfb71f49289969776432935
jamsonsor <jamsons93@gmail.com>
2023-10-05 00:03:47 -0400

    removed deprecated pyttsx3 module and replaced it with gtts module

--- pdf_to_mp3/main.py
+++ pdf_to_mp3/main.py
@@ -1,22 +1,14 @@
-import pyttsx3
-from pydub import AudioSegment
 import objc
 from pypdf import PdfReader
-from pydub.generators import say
+from gtts import gTTS
 
-text = "Hello, how are you?"
-audio = say(text)
-audio.export("output.mp3", format="mp3")
 
 pdfreader = PdfReader("flask-intro.pdf")
-#speaker = pyttsx3.init()
 
 for page_num in range(len(pdfreader.pages)):
   text = pdfreader.pages[page_num].extract_text()
   clean_text = text.strip().replace('\n', '')
   print(clean_text)
 
-# speaker.save_to_file(clean_text, 'story.mp3')
-# speaker.runAndWait()
-
-# speaker.stop()
+tts = gTTS(text=clean_text)
+tts.save("output.mp3")
