3820c37ec54892f8a9bd780b1931c8bf9f7ce4fc
jamsonsor <jamsons93@gmail.com>
2023-10-04 23:56:35 -0400

    added pydub module to practice

--- pdf_to_mp3/main.py
+++ pdf_to_mp3/main.py
@@ -1,16 +1,22 @@
 import pyttsx3
+from pydub import AudioSegment
 import objc
 from pypdf import PdfReader
+from pydub.generators import say
+
+text = "Hello, how are you?"
+audio = say(text)
+audio.export("output.mp3", format="mp3")
 
 pdfreader = PdfReader("flask-intro.pdf")
-speaker = pyttsx3.init()
+#speaker = pyttsx3.init()
 
 for page_num in range(len(pdfreader.pages)):
   text = pdfreader.pages[page_num].extract_text()
   clean_text = text.strip().replace('\n', '')
   print(clean_text)
 
-speaker.save_to_file(clean_text, 'story.mp3')
-speaker.runAndWait()
+# speaker.save_to_file(clean_text, 'story.mp3')
+# speaker.runAndWait()
 
-speaker.stop()
+# speaker.stop()
