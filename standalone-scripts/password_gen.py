856785427ca37c2eb16578592685f9d84bce3221
jamsonsor <jamsons93@gmail.com>
2023-10-11 00:21:52 -0400

    updated password_gen script to remove trailing spaces and new lines in password

--- standalone-scripts/password_gen.py
+++ standalone-scripts/password_gen.py
@@ -5,6 +5,6 @@ password_len = int(input("Write pasword length:"))
 my_password = ""
 
 for i in range(password_len):
-  my_password += random.choice(string.printable)
+  my_password += random.choice(string.printable.strip())
 
 print("Your new password:", my_password)
