cbb169d8e31cee941f805fa05a225300c8af2af3
jamsonsor <jamsons93@gmail.com>
2023-11-03 00:14:41 -0400

    added more logic to pet script

--- mini-projects/virtual_pet.py
+++ mini-projects/virtual_pet.py
@@ -8,7 +8,7 @@ class Pet:
     self.happiness = happiness
 
   def feed(self):
-    self.hunger -= 1
+    self.hunger -= 2
     print(f"{self.name} is happy, hunger reduced to {self.hunger}.")
 
   def play(self):
@@ -19,11 +19,14 @@ class Pet:
     print(f"{self.name}'s current hunger level: {self.hunger}")
     print(f"{self.name}'s current happiness level: {self.happiness}")
 
-pet = Pet("Lulu")
+pet = Pet("Rocky")
 
 while True:
+  if ( pet.hunger == 10 ):
+    print("Oh no, your pet has starved to death!")
+    break
+
   user_choice = input("> ")
-  
   if user_choice == "feed":
     pet.feed()
   elif user_choice == "play":
@@ -32,9 +35,10 @@ while True:
     pet.status()
   elif user_choice == "exit":
     break
+  else:
+    print("That's an invalid input, try again.")
 
-
-  time.sleep(1)
+  #time.sleep(0.5)
   pet.hunger += 1
 
 print("Goodbye!")
